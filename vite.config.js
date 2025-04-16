62c9a853c0e508f39bf877658be083b49b7e43ac
Jacob Stoehr <jacob@performancebrokerageservices.com>
2025-04-16 10:05:30 -0400

    Fix Vite config to point to correct index.html

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
+  root: '.', // root of your project
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, 'public/index.html')
+    },
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
